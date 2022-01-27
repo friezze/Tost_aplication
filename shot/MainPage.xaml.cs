df9e41633f4454678103e8d835376735f77c7b0b
friezze <friezze14@gmail.com>
2022-01-27 22:02:33 +0200

    previos bild

---
+++ shot/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace shot
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        int a = Preferences.Get("counter", 0);
+        public MainPage()
+        {
+            InitializeComponent();
+            TostCounter.Text = a.ToString();
+        }
+
+        private void Button_Click1(object sender, EventArgs e)
+        {
+            if (true)
+            {
+
+            }
+            a++;
+
+            TostCounter.Text = a.ToString();
+            Preferences.Set("counter", a);
+        }
+
+        private void Button_Click2(object sender, EventArgs e)
+        {
+            a = 0;
+            TostCounter.Text = a.ToString();
+            Preferences.Set("counter", a);
+        }
+    }
+}
